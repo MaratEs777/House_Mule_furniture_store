5fe8df75c0c7f0e12fe0b6edcaec1e2ea4107357
MaratEs <marik744056@gmail.com>
2023-02-15 23:49:10 +0300

    003

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,41 +5,68 @@ import { useState } from "react";
 import Items from "./Items";
 
 const products = {
- items : [
-  {
-    id : 1,
-    title: 'стул серый',
-    img : 'e4355defc43f2f664ffe659c077a2269.webp',
-    desc: 'Italian fashion',
-    category: 'chairs',
-    price: '49.99'
-  },
+  items: [
+    {
+      id: 1,
+      title: "стул серый",
+      img: "29543692fd8eab6ef2c5e10e4eeabfa7.jpg",
+      desc: "Italian fashion",
+      category: "chairs",
+      price: "49.99",
+    },
 
-  {
-    id : 2,
-    title: 'Кровать 2-м',
-    img : '6162266185.jpg',
-    desc: 'Italian fashion',
-    category: 'beds',
-    price: '157.99'
-  },
+    {
+      id: 2,
+      title: "Кровать 2-м",
+      img: "1637586622_1-idei-club-p-krovat-v-stile-loft-interer-krasivo-foto-1.jpg",
+      desc: "Italian fashion",
+      category: "beds",
+      price: "157.99",
+    },
 
-  {
-    id : 3,
-    title: 'стол кухня',
-    img : '111828c3eb8c2cf04c79c95882o9--dlya-doma-i-interera-kuhonnyj-stol-iz-massiva-dereva-v-stile-.jpg',
-    desc: 'Italian fashion',
-    category: 'tables',
-    price: '88.00'
-  }
- ]
-}
+    {
+      id: 3,
+      title: "стол кухня",
+      img: "steklyannyj-stol-v-stile-loft-2.jpg",
+      desc: "Italian fashion",
+      category: "tables",
+      price: "88.00",
+    },
+
+    {
+      id: 4,
+      title: "Диван",
+      img: "divanloft.jpg",
+      desc: "Italian fashion",
+      category: "chairs",
+      price: "220.00",
+    },
+
+    {
+      id: 5,
+      title: "Кресло",
+      img: "mb_kreslo-320348-76698.jpg",
+      desc: "Italian fashion",
+      category: "chairs",
+      price: "186.00",
+    },
+
+    {
+      id: 6,
+      title: "Тумба",
+      img: "3067941e15c0c5c65e0a97bd04fy--dlya-doma-i-interera-tumby-tumba-nizkaya-v-stile-loft.jpg",
+      desc: "Italian fashion",
+      category: "chairs",
+      price: "135.00",
+    },
+  ],
+};
 function App() {
-  const [goodsShop, setGoodShop] = useState(products)
+  const [goodsShop, setGoodShop] = useState(products);
   return (
     <div className="wrapper">
       <Header />
-      <Items product={goodsShop}/>
+      <Items product={goodsShop} />
       <Footer />
     </div>
   );
--- my-app/src/Item.jsx
+++ my-app/src/Item.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Item(props) {
   return (
-    <div>
+    <div className="item">
       <img src={"./img/" + props.item.img} alt={"no_photo"} />
       <h2>{props.item.title}</h2>
       <p>{props.item.desc}</p>

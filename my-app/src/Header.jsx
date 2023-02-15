9a62fe34ec99631d04c16145de5fa766f8de6608
MaratEs <marik744056@gmail.com>
2023-02-16 00:31:01 +0300

    004

--- my-app/src/Header.jsx
+++ my-app/src/Header.jsx
@@ -1,19 +1,32 @@
-import React from 'react'
+import { useState } from "react";
+import { FaShoppingBag } from "react-icons/fa";
 
 function Header() {
+  let [cartOpen, setCartOpen] = useState(false);
   return (
     <header>
       <div>
-        <span className='logo'>House Staff</span>
-        <ul className='nav'>
+        <span className="logo">House Staff</span>
+        <ul className="nav">
           <li>Про нас</li>
           <li>Контакты</li>
           <li>Кабинет</li>
         </ul>
+        <FaShoppingBag
+          className={`shop-cart-button ${cartOpen && "active"}`}
+          onClick={() => {
+            setCartOpen((cartOpen = !cartOpen));
+          }}
+        />
+        {cartOpen && (
+          <div className="shop-cart">
+
+          </div>
+        )}
       </div>
-      <div className='presentation'></div>
+      <div className="presentation"></div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
